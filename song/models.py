b48d950a8216e10e8d69c9205de9245739888b21
eziraa <ezratgab@gmail.com>
2024-04-27 20:30:19 +0300

    Adding song model

--- song/models.py
+++ song/models.py
@@ -12,5 +12,24 @@ class Customer(models.Model):
     favorite_songs = models.ManyToManyField(
         'Song', related_name='favorited_by', )
 
+
     def __str__(self):
         return self.name
+
+
+class Song(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    album = models.CharField(max_length=200, null=True, blank=True)
+    artist = models.CharField(max_length=200)
+    duration = models.IntegerField(null=True, blank=True)
+    release_date = models.DateField(blank=True, null=True, default=None)
+    created_at = models.DateField(
+        auto_now_add=True)
+    song_file = models.FileField(
+        upload_to='songs/', default=None, null=True, blank=True)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='songs', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
