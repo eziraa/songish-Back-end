2a207eedcbd77ff7831ff4329c9cf57b9795c82e
eziraa <ezratgab@gmail.com>
2024-05-04 10:07:18 +0300

    Creating a view to create playlist

---
+++ song/playlist_views.py
@@ -0,0 +1,27 @@
+import json
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views import View
+from .models import Playlist, Song
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .serializers.serializer import PlaylistSerializer, SongSerializer
+from rest_framework import status
+from rest_framework.response import Response
+
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from .models import Playlist, Customer
+
+
+@api_view(['POST'])
+def create_playlist(request):
+    # data = json.loads(request.body)
+    customer = Customer.objects.get(id=request.data['customer_id'])
+    playlist_data = {k: v for k, v in request.data.items()
+                     if k != 'customer_id'}
+    playlist = Playlist.objects.create(customer=customer, **playlist_data)
+    serializer = PlaylistSerializer(playlist)
+    return JsonResponse(serializer.data, status=201)
