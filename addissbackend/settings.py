868df42689a1e32f212375be3f120678edbb50ad
eziraa <ezratgab@gmail.com>
2024-05-09 12:58:02 +0300

    fix allowed host problem

--- addissbackend/settings.py
+++ addissbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = "localhost 127.0.0.1".split(" ")
 
 
 # Application definition
