dd9abd02c4461ad1a001998f898c181f08aec213
eziraa <ezratgab@gmail.com>
2024-05-10 03:29:02 +0300

    commit to use the postgres database

--- addissbackend/settings.py
+++ addissbackend/settings.py
@@ -88,7 +88,8 @@ DATABASES = {
     }
 }
 
-
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
